d4f5e891601c5f05096cea96a2218dc8aa4ca0f0
LouaiMuhammed <louaimuhammed@gmail.com>
2026-04-21 16:18:58 +0200

    Upload settings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-from routes import base
-
-load_dotenv(".env")
+from routes import base, data
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)
