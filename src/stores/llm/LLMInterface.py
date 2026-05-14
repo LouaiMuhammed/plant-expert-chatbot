2cf7e5baf19d34e588b746a540c8c42f4301980b
LouaiMuhammed <louaimuhammed@gmail.com>
2026-05-14 16:04:06 +0300

    updating LLMInterface

--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -11,13 +11,13 @@ class LLMInterface(ABC):
         pass
 
     @abstractmethod
-    def generate_text(self, prompt: str, max_output_tokens: int,
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
                       temperature: float = None):
         
         pass
 
     @abstractmethod
-    def embed_text(self, text: str, document: str):
+    def embed_text(self, text: str, document: str = None):
         pass
 
     @abstractmethod
